f96fa5fe325c7b1d9fea9d3283f535a64019d425
Aaron Lee <worldedit05@gmail.com>
2017-02-11 01:21:19 -0600

    Adjusting for Heroku deploy

--- app/actions/weatherActions.js
+++ app/actions/weatherActions.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 const isProduction = process.env.NODE_ENV === 'production';
-const config = isProduction ? require('../../config/config.sample.js') : process.env.APIXU_KEY;
+// const config = isProduction ? require('../../config/config.sample.js') : require('../../config/config.js');
 
 export function fetchWeather() {
 
   return function(dispatch) {
 
-    let apiKey = config.apixu.apiKey;
+    // let apiKey = config.apixu.apiKey;
+    let apiKey = process.env.APIXU_KEY;
 
     axios.get("http://api.apixu.com/v1/current.json?key=" + apiKey + "&q=-17.666667, 177.083333")
     .then((response) => {
